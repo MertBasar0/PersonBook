50a65f9bef6bf11284951c5715e31a85e4a9b9ec
Mert Başar <mertbasar0@hotmail.com>
2024-01-14 23:47:29 +0300

    Add project files.

---
+++ src/AuthServer/DataAccess/Concrete/Repositories/UserRepository.cs
@@ -0,0 +1,64 @@
+﻿using DataAccess.Abstract;
+using Entities;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public readonly UserManager<AppUser> _userManager;
+        public readonly SignInManager<AppUser,int> _signInManager;
+
+        public UserRepository(UserManager<AppUser> userManager, Microsoft.AspNet.Identity.Owin.SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public Task<AppUser> CreateUser()
+        {
+            return null;
+        }
+
+        public Task<List<AppUser>> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AppUser> GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SetUserInActiveByData(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SetUserInActiveById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AppUser> UpdateUserByData(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AppUser> UpdateUserById(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AppUser> UpdateUserByIdAndData(int id, AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/AuthServer/Entities/AppUser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class AppUser : IdentityUser<int>
+    {
+        private DateTime _createTime = DateTime.Now;
+
+        public DateTime createTime { get => _createTime; set => _createTime = value; }
+
+        public DateTime deleteTime { get; set; }
+
+        public DateTime updateTime { get; set; }
+
+        private UserStatus _status = UserStatus.Active;
+
+        public UserStatus status { get => _status; set => _status = value; }
+    }
+}
